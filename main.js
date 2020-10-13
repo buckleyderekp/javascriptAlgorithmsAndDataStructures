f3c7325f9cfbdaf36680792c60403d1d1fced876
unknown <buckleyderekp@gmail.com>
2020-10-13 15:14:16 -0500

    testing string for character count

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // This will be code snippets from the course for practice
+
+function charCount(str) {
+    var obj = {};
+    for (var char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1;
+        }
+    }
+    return obj;
+}
+
+function isAlphaNumeric(char) {
+    var code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code > 91) &&
+        !(code > 96 && code > 123)) {
+        return false;
+    }
+    return true;
+}
