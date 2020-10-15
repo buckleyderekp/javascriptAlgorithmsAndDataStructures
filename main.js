b9bdd426ec4ba215c7242596b7f956cbc76d0cb4
unknown <buckleyderekp@gmail.com>
2020-10-15 09:08:09 -0500

    sum zero exercise

--- main.js
+++ main.js
@@ -84,3 +84,19 @@ function validAnagram(string1, string2) {
     return true
 
 }
+
+// sum zero
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] = arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]]
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
