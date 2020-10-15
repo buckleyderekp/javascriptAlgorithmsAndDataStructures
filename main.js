e23ff5c550df84a739b067143ecd3bea0d4deb8a
unknown <buckleyderekp@gmail.com>
2020-10-15 08:49:09 -0500

    anagram exercise

--- main.js
+++ main.js
@@ -49,13 +49,38 @@ function same2(arr1, arr2) {
     for (let val of arr2) {
         frequncyCounter2[val] = (freuquncyCounter2[val] || 0) + 1
     }
-    for (let key in frequencyCOunter1) {
+    for (let key in frequencyCounter1) {
         if (!(key ** 2 in frequncyCOunter2)) {
             return false
         }
-        if (frequencyCOunter2[key ** 2] !== frequencyCounter1[key]) {
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
             return false
         }
     }
     return true
+}
+
+//Anagrams
+function validAnagram(string1, string2) {
+    if (string1.length !== string2.length) {
+        return false
+    }
+    let lookup = {}
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i]
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i]
+
+        if (!lookup[letter]) {
+            return false;
+        }
+        else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true
+
 }
