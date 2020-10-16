27ec500d16ed6078c44163d08883cbe912907545
unknown <buckleyderekp@gmail.com>
2020-10-16 10:52:45 -0500

    sliding window

--- main.js
+++ main.js
@@ -113,3 +113,22 @@ function countUniqueValues(arr) {
     }
     return i + 1;
 }
+
+// sliding window 
+//version that works but is not best
+function maxSubArraySum(arr, num){
+    if (num > arr.length){
+        return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num +1; i++){
+        temp = 0;
+        for (let j = 0; j <num; j++){
+            temp += arr[i + j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+    }
+    return max;
+}
